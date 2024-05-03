615aaa9abdcee820a0c6f0581cd8bf4e7a70fb0c
Daniel Costa <daniel.c.dan@gmail.com>
2024-05-03 11:54:55 -0300

    Add news files and Codes: Material UI Components

---
+++ src/theme.ts
@@ -0,0 +1,17 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#38598b',
+    },
+    secondary: {
+      main: '#9fd3c7',
+    },
+  },
+  typography: {
+    fontFamily: "Helvetica Neue"
+  }
+});
+
+export default theme;
